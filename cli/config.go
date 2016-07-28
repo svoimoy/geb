1da303adaeaa93a67865a865f4ef6f160c7b9d5c
Tony Worm <afworm@us.ibm.com>
2016-07-28 01:05:02 -0600

    got some new code

---
+++ cli/config.go
@@ -0,0 +1,16 @@
+package templates
+
+// geb-cli:
+//     A cobra/viper based CLI dsl for you projects.
+
+// HOFSTADTER: line in the sand
+// You may add, edit, and whatever below this line.
+// Use the DSL for modifications above here.
+//
+// Exception, do not change the '''templateConfig''' variable name.
+// Unexception, you may set it equal to anything you'd like!
+
+var templateConfig = `
+
+
+`
