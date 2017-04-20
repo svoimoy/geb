39d80eb3111a0e5cd275fd5cb7072f90033920ac
Tony Worm <afworm@us.ibm.com>
2017-04-19 23:32:59 -0600

    adding fields for more grainular configurability in template rendering contexts

--- engine/gen/TemplateRenderPair.go
+++ engine/gen/TemplateRenderPair.go
@@ -23,14 +23,19 @@ About:
 Where's your docs doc?!
 */
 type TemplateRenderPair struct {
-	When   string `json:"when" xml:"when" yaml:"when" form:"when" query:"when" `
-	Unless string `json:"unless" xml:"unless" yaml:"unless" form:"unless" query:"unless" `
-	In     string `json:"in" xml:"in" yaml:"in" form:"in" query:"in" `
-	Out    string `json:"out" xml:"out" yaml:"out" form:"out" query:"out" `
+	When    string   `json:"when" xml:"when" yaml:"when" form:"when" query:"when" `
+	Unless  string   `json:"unless" xml:"unless" yaml:"unless" form:"unless" query:"unless" `
+	Field   string   `json:"field" xml:"field" yaml:"field" form:"field" query:"field" `
+	Flatten int      `json:"flatten" xml:"flatten" yaml:"flatten" form:"flatten" query:"flatten" `
+	In      string   `json:"in" xml:"in" yaml:"in" form:"in" query:"in" `
+	Out     string   `json:"out" xml:"out" yaml:"out" form:"out" query:"out" `
+	Extra   []string `json:"extra" xml:"extra" yaml:"extra" form:"extra" query:"extra" `
 }
 
 func NewTemplateRenderPair() *TemplateRenderPair {
-	return &TemplateRenderPair{}
+	return &TemplateRenderPair{
+		Extra: []string{},
+	}
 }
 
 // HOFSTADTER_BELOW
