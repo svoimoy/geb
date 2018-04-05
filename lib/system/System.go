1ff2cc9998f7fb8af21bce9c189a861c9124e121
Tony Worm <verdverm@gmail.com>
2018-04-05 10:40:03 -0600

    add some git libraries

--- lib/system/System.go
+++ lib/system/System.go
@@ -1,8 +1,12 @@
 package system
 
 import (
-// HOFSTADTER_START import
-// HOFSTADTER_END   import
+	// HOFSTADTER_START import
+	"fmt"
+
+	"github.com/sergi/go-diff/diffmatchpatch"
+	"gopkg.in/src-d/go-git.v4"
+	// HOFSTADTER_END   import
 )
 
 // HOFSTADTER_START const
@@ -15,3 +19,23 @@ import (
 // HOFSTADTER_END   init
 
 // HOFSTADTER_BELOW
+
+const (
+	text1 = "Lorem ipsum dolor."
+	text2 = "Lorem dolor sit amet."
+)
+
+func keepImportsAndDeps() {
+	r, err := git.PlainClone("directory", false, &git.CloneOptions{
+		URL: "https://github.com/hofstadter-io/dsl-library",
+	})
+
+	fmt.Println("go-git", r, err)
+
+	dmp := diffmatchpatch.New()
+
+	diffs := dmp.DiffMain(text1, text2, false)
+
+	fmt.Println(dmp.DiffPrettyText(diffs))
+
+}
